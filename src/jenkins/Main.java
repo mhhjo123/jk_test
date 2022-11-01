43829622c9f35783ea473eea78cceff7a0f09fbd
JAVA_TEST <mskim4348>
2022-11-01 15:18:10 +0900

    15:17

--- src/jenkins/Main.java
+++ src/jenkins/Main.java
@@ -3,8 +3,8 @@ package jenkins;
 public class Main {
 
 	public static void main(String[] args) {
-		// build test, 11-01 14:32
-		System.out.println("Hello World!!!!");
+		// build test, 11-01 15:14
+		System.out.println("Hello World!!");
 	}
 
 }
